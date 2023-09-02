39dca310b15867d9e68737447fe9bd964cd0d7c8
Khai Do <khaitroyy@gmail.com>
2023-09-02 00:50:16 -0500

    edit

--- car.py
+++ car.py
@@ -1,23 +1,24 @@
-from turtle import Turtle 
+from turtle import Turtle, Screen
 import random
 
 car_colors = ["black", "green", "blue", "orange", "red"]
 
-
 class Car(Turtle):
-    def __init__(self):
+    def __init__(self, x_cor, y_cor):
         super().__init__()
         self.shape("square")
-        self.shapesize(1,2.5)
+        self.shapesize(1, 2.5)
         self.color(random.choice(car_colors))
-        self.penup()    
+        self.penup()
+        self.goto(x_cor, y_cor)
 
     def move_left(self):
         self.backward(10)
 
-    def create_car(self):
-        y_position = []
-        x_position = 390
-        for y_cor in range(-240,240,10):
-            y_position.append(y_cor)
 
+# Example usage:
+screen = Screen()
+screen.setup(width=800, height=600)
+cars_list = create_cars()
+
+# This will create cars at different y_cor positions within the specified range.
---
+++ test.py
@@ -0,0 +1,6 @@
+from car import Car
+from turtle import Turtle, Screen 
+
+screen = Screen()
+screen.setup(width=800, height=600)
+cars_list = create_cars()
